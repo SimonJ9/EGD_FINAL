af2d2e6cff7cc91c59cfa4d008fc7556b529745e
simonj-pc <simonj142857@gmail.com>
2018-06-03 15:24:18 +0800

    Change Name to "The Dawn". Add part of todo list. update engine info

---
+++ TheDawn/Intermediate/Build/Win64/UE4Editor/Inc/egdFinal/egdFinalGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EGDFINAL_egdFinalGameModeBase_generated_h
+#error "egdFinalGameModeBase.generated.h already included, missing '#pragma once' in egdFinalGameModeBase.h"
+#endif
+#define EGDFINAL_egdFinalGameModeBase_generated_h
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_RPC_WRAPPERS
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAegdFinalGameModeBase(); \
+	friend EGDFINAL_API class UClass* Z_Construct_UClass_AegdFinalGameModeBase(); \
+public: \
+	DECLARE_CLASS(AegdFinalGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/egdFinal"), NO_API) \
+	DECLARE_SERIALIZER(AegdFinalGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAegdFinalGameModeBase(); \
+	friend EGDFINAL_API class UClass* Z_Construct_UClass_AegdFinalGameModeBase(); \
+public: \
+	DECLARE_CLASS(AegdFinalGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/egdFinal"), NO_API) \
+	DECLARE_SERIALIZER(AegdFinalGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AegdFinalGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AegdFinalGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AegdFinalGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AegdFinalGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AegdFinalGameModeBase(AegdFinalGameModeBase&&); \
+	NO_API AegdFinalGameModeBase(const AegdFinalGameModeBase&); \
+public:
+
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AegdFinalGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AegdFinalGameModeBase(AegdFinalGameModeBase&&); \
+	NO_API AegdFinalGameModeBase(const AegdFinalGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AegdFinalGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AegdFinalGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AegdFinalGameModeBase)
+
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_12_PROLOG
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_RPC_WRAPPERS \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_INCLASS \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	egdFinal_Source_egdFinal_egdFinalGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID egdFinal_Source_egdFinal_egdFinalGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
